7bfde11f92ad7f2db6f1c9c43bc6b1f86cb17b60
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-04-13 20:05:49 +0530

    created new component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.Component.css']
+})
+
+export class ServerComponent{
+
+}
