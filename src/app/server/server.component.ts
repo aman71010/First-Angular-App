58055ac672ada7c3a059d795d13bbe08de6a5532
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-06-01 16:09:31 +0530

    added new components, uses data-binding and directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,9 +3,16 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
-    styleUrls: ['./server.Component.css']
+    styleUrls: ['./server.component.css']
 })
 
 export class ServerComponent{
-
+    serverId = 20;
+    serverStatus = Math.random() < 0.5 ? "offline": "online";
+    getServerStatus(){
+        return this.serverStatus;
+    }
+    getColor(){
+        return this.serverStatus === "offline" ? "red": "green";
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreatedStatus = "No server is created!";
+  serverName = "";
+  createNewServer = false;
+  servers = ['TestServer', 'TestServer2'];
 
+  constructor(){
+    setTimeout( () => {
+      this.allowNewServer = true;
+    }, 3000)
+  }
+
+  onCreateServer(){
+    this.createNewServer = true;
+    this.serverCreatedStatus = "server was created. Its name is " + this.serverName;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServer(event){
+    this.serverName = event.target.value;
+  }
 }
